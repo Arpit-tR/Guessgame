2df42cc0eb59a0a9a2ef9c53b1277f89c9b88dc1
Arpit Sadhukhan <76998325+Arpit-tR@users.noreply.github.com>
2024-04-15 12:47:41 +0530

    adding better script file

--- script.js
+++ script.js
@@ -1,86 +1,89 @@
-'use strict';
+"use strict";
 
 let number = Math.trunc(Math.random() * 20) + 1;
-//document.querySelector('.number').textContent = number;
+console.log(number);
+
 const displayMessage = function (message) {
-  document.querySelector('.message').textContent = message;
+  document.querySelector(".message").textContent = message;
 };
 
 let highscore = 0;
 let score = 20;
 
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
+var inputField = document.querySelector(".guess");
+inputField.disabled = false;
 
-  if (guess <= 20) {
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+
+  if (guess <= 20 && guess > 0) {
     // when there is no input
     if (!guess) {
-      // document.querySelector('.message').textContent = '⛔ No Number';
-      displayMessage('⛔ No Number');
+      displayMessage("⛔ No Number");
 
       // when right guess
     } else if (guess === number) {
-      // document.querySelector('.message').textContent = '🎉 corrent number!';
-      displayMessage('🎉 corrent number!');
-      document.querySelector('body').style.backgroundColor = '#47ffb6';
-      document.querySelector('.number').style.width = '50rem';
-      document.querySelector('.number').style.fontSize = '10rem';
-      document.querySelector('.number').textContent = number;
+      displayMessage("🎉 corrent number!");
+      inputField.disabled = true;
+      document.querySelector("body").style.backgroundColor = "#47ffb6";
+      document.querySelector(".number").style.width = "50rem";
+      document.querySelector(".number").style.fontSize = "10rem";
+      document.querySelector(".number").textContent = number;
+      document.querySelector(".number").style.borderColor = "#000";
+      document.querySelector(".number").style.color = "#000";
+      document.querySelector(".check").style.backgroundColor = "#eee";
+      document.querySelector(".check").style.cursor = "default";
+      document.querySelector(".check").textContent = "🎉";
+      document.querySelector(".head").style.color = "#000000";
+      document.querySelector(".between").style.color = "#000000";
+      document.querySelector(".lable-score").style.color = "#000";
+      document.querySelector(".lable-highscore").style.color = "#000";
+      document.querySelector(".message").style.color = "#000";
+      document.querySelector(".guess").style.color = "#000";
+      document.querySelector(".guess").style.borderColor = "#000";
+
       if (highscore <= score) {
         highscore = score;
-        document.querySelector('.highscore').textContent = highscore;
+        document.querySelector(".highscore").textContent = highscore;
       }
     } else if (guess !== number) {
       if (score <= 1) {
-        // document.querySelector('.message').textContent = '👎 try again';
-        displayMessage('👎 try again');
-        document.querySelector('.score').textContent = 0;
+        displayMessage("👎 try again");
+        inputField.disabled = true;
+        document.querySelector(".check").style.cursor = "default";
+        document.querySelector(".check").style.backgroundColor = "#eee";
+        document.querySelector(".score").textContent = 0;
       } else {
-        // document.querySelector('.message').textContent =
-        //   guess < number ? '📉 too lower' : '📈 too High';
-
-        displayMessage(guess < number ? '📉 too lower' : '📈 too High');
+        displayMessage(guess < number ? "📉 too lower" : "📈 too High");
         score--;
-        document.querySelector('.score').textContent = score;
+        document.querySelector(".score").textContent = score;
       }
     }
-
-    // guess lower than number
-    // } else if (guess < number) {
-    //   if (score <= 1) {
-    //     document.querySelector('.message').textContent = '👎 try again';
-    //     document.querySelector('.score').textContent = 0;
-    //   } else {
-    //     document.querySelector('.message').textContent = '📉 too lower';
-    //     score--;
-    //     document.querySelector('.score').textContent = score;
-    //   }
-    //   //guess higher than number
-    // } else if (guess > number) {
-    //   if (score <= 1) {
-    //     document.querySelector('.message').textContent = '👎 try again';
-    //     document.querySelector('.score').textContent = 0;
-    //   } else {
-    //     document.querySelector('.message').textContent = '📈 too High';
-    //     document.querySelector('.score').textContent = score;
-    //     score--;
-    //   }
   } else {
-    // document.querySelector('.message').textContent = 'Greater than 20 ';
-    displayMessage('Greater than 20');
+    displayMessage("not in range 1-20");
   }
 });
 
-document.querySelector('.again').addEventListener('click', function () {
+document.querySelector(".again").addEventListener("click", function () {
   score = 20;
   number = Math.trunc(Math.random() * 20) + 1;
-  // document.querySelector('.message').textContent = 'Start Guessing...';
-  displayMessage('Start Guessing...');
-  document.querySelector('body').style.backgroundColor = '#315374';
-  document.querySelector('.score').textContent = score;
-  document.querySelector('.number').textContent = '?';
-  document.querySelector('.guess').value = '';
-  document.querySelector('.number').style.width = '30rem';
-  document.querySelector('.number').style.fontSize = '7rem';
-  // document.querySelector('.highscore').textContent = highscore;
+  inputField.disabled = false;
+  displayMessage("Start Guessing...");
+  document.querySelector("body").style.backgroundColor = "#3b78b4";
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".number").style.borderColor = "";
+  document.querySelector(".number").style.width = "20rem";
+  document.querySelector(".number").style.fontSize = "7rem";
+  document.querySelector(".check").style.backgroundColor = "";
+  document.querySelector(".check").style.cursor = "pointer";
+  document.querySelector(".check").textContent = "Check!";
+  document.querySelector(".head").style.color = "";
+  document.querySelector(".between").style.color = "";
+  document.querySelector(".lable-score").style.color = "";
+  document.querySelector(".lable-highscore").style.color = "";
+  document.querySelector(".message").style.color = "";
+  document.querySelector(".guess").style.color = "";
+  document.querySelector(".guess").style.borderColor = "";
+  document.querySelector(".guess").value = "";
 });
